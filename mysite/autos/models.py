1a4ad57f4eca7a0e0c147452c18178a8fcdb55df
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-07-15 10:25:37 -0300

    help_text mais generalista
    Antes estava como "Entre com uma montadora, tipo ASX", mas como o código serve tanto para CREATE quanto para o UPDATE, é preciso que seja algo mais geral, como "Digite um item."

--- mysite/autos/models.py
+++ mysite/autos/models.py
@@ -4,14 +4,16 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 class Make(models.Model):
-    name = models.CharField(max_length=220,
-        help_text="Entre com uma montadora, tipo ASX.",
-        validators=[MinLengthValidator(2, "Tem que ser maior que 1 caractere!")])
+    name = models.CharField(
+        max_length=220,
+        help_text="Digite um item.",
+        validators=[MinLengthValidator(2, "Tem que ser maior que 1 caractere!")]
+        )
     
     def __str__(self):
         """String que representa o objeto do modelo"""
         return self.name
-    
+
 
 class Auto(models.Model):
     nickname = models.CharField(max_length=200, 
