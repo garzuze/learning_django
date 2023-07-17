7b7bcc43089cbd7f6c1580170a11cc630350e502
Lucas Garzuze Cordeiro <118022446+lucas-garzuze-cordeiro@users.noreply.github.com>
2023-07-17 11:11:17 -0300

    Forms não é mais necessário
    Como transformei tudo em generic views, eu não preciso mais do forms.py porque as views já fazem isso pra mim!

--- mysite/autos/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from autos.models import Make
-
-
-# Criar a classe de formulário que cria as montadoras
-class MakeForm(ModelForm):
-    class Meta:
-        model = Make
-        fields = '__all__'
